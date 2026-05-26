1bcd6bf3476cf707e719a9947da276c76372f9d7
Jeppe Roulund <jeppe.roulund@sparrowquantum.com>
2026-05-26 16:24:40 +0200

    Feat: Make downstream dependency easier
    Flake now outputs a lib with a python override overlay and a function
    for building the qudi core package including downstream overrides

--- flake.nix
+++ flake.nix
@@ -16,106 +16,94 @@
         pkgs = nixpkgs.legacyPackages.${system};
         inherit (pkgs) lib;
 
-        python = pkgs.python313.override {
-          packageOverrides = _: super: {
-            # Project needs rpyc 5.*.* but nixpkgs is currently on >6.0.0
-            rpyc = super.rpyc.overridePythonAttrs (_: rec {
-              version = "5.3.1";
-              src = pkgs.fetchFromGitHub {
-                owner = "tomerfiliba";
-                repo = "rpyc";
-                tag = version;
-                hash = "sha256-2b6ryqDqZPs5VniLhCwA1/c9+3CT+JJrr3VwP3G6tpY=";
-              };
-            });
-          };
-        };
-        pyPkgs = python.pkgs;
+        pythonOverrides = _: super: {
+          # Project needs rpyc 5.*.* but nixpkgs is currently on >6.0.0
+          rpyc = super.rpyc.overridePythonAttrs (_: rec {
+            version = "5.3.1";
+            src = pkgs.fetchFromGitHub {
+              owner = "tomerfiliba";
+              repo = "rpyc";
+              tag = version;
+              hash = "sha256-2b6ryqDqZPs5VniLhCwA1/c9+3CT+JJrr3VwP3G6tpY=";
+            };
+          });
 
-        # Fysom does not exist in nixpkgs, so we build it ourselves
-        fysom = pyPkgs.buildPythonPackage rec {
-          pname = "fysom";
-          version = "2.1.6";
-          format = "setuptools";
-          src = pkgs.fetchPypi {
-            inherit pname version;
-            sha256 = "sha256-42F7efGSIMcrHH5p1NbCo2qAzb6A9N973rq1q0yJO/U=";
-          };
-          doCheck = false;
-          meta = {
-            description = "Finite State Machine for Python";
-            homepage = "https://github.com/mriehl/fysom";
-            license = lib.licenses.mit;
+          # Fysom does not exist in nixpkgs, so we build it ourselves
+          fysom = super.buildPythonPackage rec {
+            pname = "fysom";
+            version = "2.1.6";
+            format = "setuptools";
+            src = pkgs.fetchPypi {
+              inherit pname version;
+              sha256 = "sha256-42F7efGSIMcrHH5p1NbCo2qAzb6A9N973rq1q0yJO/U=";
+            };
+            doCheck = false;
+            meta = {
+              description = "Finite State Machine for Python";
+              homepage = "https://github.com/mriehl/fysom";
+              license = lib.licenses.mit;
+            };
           };
         };
 
-        qudiCore = pyPkgs.buildPythonPackage {
-          pname = "qudi-core";
-          version = lib.strings.removeSuffix "\n" (builtins.readFile ./VERSION);
-          pyproject = true;
-          src = ./.;
+        mkQudiPackage = {
+          pkgs,
+          python,
+        }:
+          python.pkgs.buildPythonPackage {
+            pname = "qudi-core";
+            version = lib.strings.removeSuffix "\n" (builtins.readFile ./VERSION);
+            pyproject = true;
+            src = ./.;
 
-          build-system = with pyPkgs; [
-            setuptools
-            setuptools-scm
-            wheel
-          ];
+            build-system = with python.pkgs; [
+              setuptools
+              setuptools-scm
+              wheel
+            ];
 
-          dependencies = with pyPkgs; [
-            cycler
-            entrypoints
-            fysom
-            gitpython
-            jupyter
-            jupytext
-            lmfit
-            matplotlib
-            numpy
-            pyqtgraph
-            pyside6
-            rpyc
-            ruamel-yaml
-            scipy
-            jsonschema
-            qtconsole
-          ];
+            dependencies = with python.pkgs; [
+              cycler
+              entrypoints
+              fysom
+              gitpython
+              jupyter
+              jupytext
+              lmfit
+              matplotlib
+              numpy
+              pyqtgraph
+              pyside6
+              rpyc
+              ruamel-yaml
+              scipy
+              jsonschema
+              qtconsole
+            ];
 
-          nativeBuildInputs = [
-            pkgs.qt6.wrapQtAppsHook
-          ];
-          buildInputs = [
-            pkgs.qt6.qtsvg # SVG QIcon support. Propagates qtbase
-          ];
+            nativeBuildInputs = [
+              pkgs.qt6.wrapQtAppsHook
+            ];
+            buildInputs = [
+              pkgs.qt6.qtsvg # SVG QIcon support. Propagates qtbase
+            ];
 
-          # Wrap app to set environment variables for Qt plugins
-          postFixup = ''
-            wrapQtApp "$out/bin/qudi"
-          '';
+            # Wrap app to set environment variables for Qt plugins
+            postFixup = ''
+              wrapQtApp "$out/bin/qudi"
+            '';
 
-          pythonImportsCheck = ["qudi"];
+            pythonImportsCheck = ["qudi"];
 
-          meta = {
-            description = "A framework for modular measurement applications";
-            homepage = "https://github.com/SparrowQuantum/qudi-core-sparrow";
-            license = lib.licenses.lgpl3;
+            meta = {
+              description = "A framework for modular measurement applications";
+              homepage = "https://github.com/SparrowQuantum/qudi-core-sparrow";
+              license = lib.licenses.lgpl3;
+            };
           };
-        };
 
-        # Simple script that ensures the qudi Jupyter kernel is installed before launching
-        qudiLauncher = pkgs.writeScriptBin "qudi-launch" ''
-          #!${devEnv}/bin/python
-
-          import os
-          import subprocess
-          import sys
-
-          subprocess.run(
-              ["${devEnv}/bin/python", "-m", "qudi.core.qudikernel", "ensure"],
-              check=True,
-          )
-
-          os.execv("${qudiCore}/bin/qudi", ["${qudiCore}/bin/qudi", *sys.argv[1:]])
-        '';
+        python = pkgs.python313.override {packageOverrides = pythonOverrides;};
+        qudiCore = mkQudiPackage {inherit pkgs python;};
 
         devEnv = python.withPackages (_: [qudiCore]);
 
@@ -130,14 +118,17 @@
         packages = {
           default = qudiCore;
           qudi-core = qudiCore;
-          fysom = fysom;
-          python = python;
+        };
+
+        lib = {
+          pythonOverrides = pythonOverrides;
+          mkQudiPackage = mkQudiPackage;
         };
 
         apps = {
           default = {
             type = "app";
-            program = "${qudiLauncher}/bin/qudi-launch";
+            program = "${qudiCore}/bin/qudi";
             meta = {
               description = "Launch Qudi-core";
             };
@@ -162,7 +153,6 @@
             pkgs.gh
             pkgs.fd
             devEnv
-            qudiLauncher
           ];
         };
 
